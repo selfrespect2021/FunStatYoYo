0105e884bf9f9b9c758138992066ecee5fbf2fc9
selfrespect2021 <88738551+selfrespect2021@users.noreply.github.com>
2021-08-10 20:09:35 +0100

    Update streamlit_stats.py

--- streamlit_stats.py
+++ streamlit_stats.py
@@ -20,7 +20,7 @@ selected_feature="Moyenne Gen"
 
 
 useful_col=first_last_name+all_subjects
-st.title("YouyouStats, what else!")
+st.title("احصائيات المستوى السادس بطدرسة طه حسين")
 
 def getComparisonFeature(df):
     if st.checkbox("Choisisez un seul critere pour le choix des groupes."):
